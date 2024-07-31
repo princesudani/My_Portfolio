eef538da7c48753d57bd6677c613289c6eb1ac9c
princesudani <princesudani0610@gmail.com>
2024-07-31 19:41:20 +0530

    vite package change

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import staticSite from 'vite-plugin-static-site';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    staticSite({
-      staticDir: 'dist', // The directory where the built files are located
-      buildOptions: {
-        outDir: 'build', // The output directory for the build
-      },
-    }),
-  ],
-  base: '/My_Portfolio/', // Replace 'your-repo-name' with the name of your GitHub repo
-});
+  plugins: [react()],
+})
